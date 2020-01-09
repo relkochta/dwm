e5b4428a4a72142c714b8ad1386131eb401965f5
Ryan El Kochta <relkochta@lightn1ng.space>
2020-01-09 11:43:26 -0500

    Thing2

--- config.def.h
+++ config.def.h
@@ -78,8 +78,8 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ 0, XKB_KEY_XF86MonBrightnessUp, spawn, {.v = brightupcmd } },
-	{ 0, XKB_KEY_XF86MonBrightnessDown, spawn, {.v = brightdowncmd } },
+	{ 0, XF86MonBrightnessUp, spawn, {.v = brightupcmd } },
+	{ 0, XF86MonBrightnessDown, spawn, {.v = brightdowncmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
