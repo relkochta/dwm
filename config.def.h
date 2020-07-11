c715868620084c47e9c94ae6b51815cc3bd01bf6
Ryan El Kochta <relkochta@protonmail.com>
2020-07-11 19:43:34 -0400

    movestack

--- config.def.h
+++ config.def.h
@@ -76,6 +76,7 @@ static const char *brightdowncmd[] = { "/home/relkochta/.scripts/bright-down.sh"
 static const char *volupcmd[] = { "/home/relkochta/.scripts/vol-up.sh", NULL };
 static const char *voldowncmd[] = { "/home/relkochta/.scripts/vol-down.sh", NULL };
 
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -91,6 +92,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
